650cd50ac886841ab32d1b42960527d352c79486
Sven <sven.vander.mierde@gmail.com>
2021-10-12 14:29:19 +0200

    added reset button

--- JS/script.js
+++ JS/script.js
@@ -4,12 +4,16 @@ console.log(buttons);
 
 for (i=0; i<buttons.length;i++) {
     var button = buttons[i];
-    button.setAttribute("disabled", true);
+    if(button.id !=="reset"){
+        button.setAttribute("disabled", true);
+    }
+
 }
+
 
 //defining variables on top
 //Defining first value of the clicker
-let pint = 100;
+let pint = 1;
 let boost = 1
 pint *= boost;
 document.getElementById("income").innerText = pint;
@@ -17,13 +21,16 @@ document.getElementById("multiplier1").innerText = '2';
 //defining the score
 let target = document.getElementById("score");
 let interval;
-let boost1price = 1000;
 let score = 0;
-let counter = 0;
+let multiCounter = 0;
+let autoCounter = 0;
+let boostCounter = 0;
 let price1 = 100;
 let priceAuto1 = 25;
 let boost1Price = 1000;
 
+
+checkValues();
 //iClick: werkt!
 //the event listener for clicking the button
 document.getElementById("click").addEventListener("click", function() {
@@ -45,16 +52,13 @@ document.getElementById("multiply1").addEventListener("click", function() {
         //displaying the reduction as well.
         target.innerText = score;
         //how many times has the multiplier been used.
-        counter += 1;
+        multiCounter++;
         //multiplying the price of the beer
-
         pint *= 2;
-        console.log(pint)
         //multiplying the price increase of the multiplier
-        price1 *= counter+1;
+        price1 *= multiCounter+1;
         //visualising
         document.getElementById("multiply1price").innerText = price1;
-        document.getElementById("multiply1price").innerText += " Multiplier: x" + 2;
         document.getElementById("income").innerText = pint;
         //checking values for button disables
         checkValues();
@@ -66,7 +70,10 @@ document.getElementById("multiply1").addEventListener("click", function() {
 document.getElementById("autoclick1").addEventListener("click",function() {
     console.log("a")
     if (score >= priceAuto1) {
+        autoCounter++;
         score -= priceAuto1;
+        priceAuto1 *= autoCounter + 1;
+        document.getElementById('autoclick1price').innerText = priceAuto1;
         target.innerText = score;
         setInterval(function iAutoClick() {
             score += pint;
@@ -83,8 +90,11 @@ document.getElementById("autoclick1").addEventListener("click",function() {
 
 document.getElementById("boost1").addEventListener("click", function (){
     if (score >= boost1Price){
+        boostCounter++;
         score-=boost1Price;
         target.innerText = score;
+        boost1Price *= boostCounter+1;
+        document.getElementById('boost1price').innerText = boost1Price;
         let timer=0;
         document.getElementById("boost1").setAttribute("disabled", "");
         console.log("a");
@@ -94,11 +104,16 @@ document.getElementById("boost1").addEventListener("click", function (){
             timer++;
             document.getElementById('timer').innerText = 30 - timer + ' sec';
             document.getElementById("timer").innerText = 30 - timer + " s";
+            document.getElementById("income").style.color = "darkred";
+            document.getElementById("income").innerText = pint;
             console.log(timer);
             if (timer === 30) {
                 boost = 1;
                 pint *= boost / 2;
                 document.getElementById("boost1").removeAttribute("disabled", "");
+                document.getElementById("income").style.color = "#333333";
+                document.getElementById("income").innerText = pint;
+                checkValues();
                 clearInterval(interval)
             }
         }, 1000)
